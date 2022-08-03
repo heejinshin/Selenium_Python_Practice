6ef661d2f519313547d39187409ad19a85b9a562
신희진 <heejinshin02@gmail.com>
2022-08-03 17:29:58 +0900

    practice1

--- IdeaProjects/practice1/src/ex1/StudentEx1.java
+++ IdeaProjects/practice1/src/ex1/StudentEx1.java
@@ -1,9 +1,4 @@
-package ex1;
+import ex1.StudentEx2;
+
 import java.util.Scanner;
 
-public class StudentEx1 {
-
-    public static void main(String[] args) {}
-    Scanner sc = new Scanner(System.in);
-    StudentEx2 Student1 = new StudentEx2();
-}
--- IdeaProjects/practice1/src/ex1/StudentEx2.java
+++ IdeaProjects/practice1/src/ex1/StudentEx2.java
@@ -2,7 +2,7 @@ package ex1;
 
 import java.util.Scanner;
 
-class StudentEx2{
+public class StudentEx2{
     private String name;
     private int id;
 
@@ -18,6 +18,30 @@ class StudentEx2{
     int getId() {
         return this.id;
     }
+
+    public class StudentEx1 {
+
+        public void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            StudentEx2 student1 = new StudentEx2();
+
+            System.out.println("-- 회원가입 --");
+            System.out.println("-- 학과 입력 --");
+
+            String name = sc.next();
+            student1.setName(name);
+
+            System.out.println("학번 입력 :");
+            int id = sc.nextInt();
+            student1.setId(id);
+
+            System.out.println("학생1의 학과:" + student1.getName()+", 학번: " + student1.getId());
+
+        }
+
+    }
 }
+
+
 
 
